d34701c1e0bdcdea8cd9f0d5106b1bb45dcff63d
Ilyas Khallouki <khalloukielias@gmail.com>
2024-10-31 13:01:02 +0100

    Using bs4 in the tree builder

--- tree_builder.py
+++ tree_builder.py
@@ -1,11 +1,13 @@
+from element import Element
+
 class TreeBuilder:
-    def __init__(self, page):
-        self.page = page
-        self.element_tree = None
+    def __init__(self):
+        self.element_tree: Element = None
         self.depth = 0
 
-    def parse_tree(self):
+    def parse_tree(self, page):
         self.element_tree = None
+        self.get_depth()
     
     def get_depth(self):
         self.depth = 0
