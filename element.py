1723d46679b9d94499c6d30266134e7231ba9ff5
Ilyas Khallouki <khalloukielias@gmail.com>
2024-11-02 14:01:46 +0100

    Adding a better print

--- element.py
+++ element.py
@@ -11,9 +11,30 @@ class Element:
         
         self.content = content
         self.tag = tag
-        self.attr = attr
+        self.attr = attr if attr is not None else {}
         self.parent = parent
-        self.children = children
+        self.children = children if children is not None else []
         self.depth = depth
         self.is_self_closing = is_self_closing
-    
+
+    # A better print for the element
+    def __str__(self):
+        indent = "  " * self.depth  # Indentation based on depth
+        attrs = ', '.join(f'{key}="{value}"' for key, value in self.attr.items())
+        attrs_str = f" [{attrs}]" if attrs else ""
+        
+        content_str = f": {self.content}" if self.content else ""
+        
+        # Print the element and its children
+        result = f"{indent}<{self.tag}{attrs_str}{content_str}>"
+        
+        for child in self.children:
+            result += f"\n{str(child)}"  # Recursive call for children
+        
+        if self.children:  # If there are children, close the tag
+            result += f"\n{indent}</{self.tag}>"
+        
+        return result
+
+    def __repr__(self):
+        return f"Element(tag='{self.tag}', content='{self.content}', attributes={self.attr}, depth={self.depth})"
